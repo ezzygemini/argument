12bc1369cc3249504395de8d4f3a66c1f56ca223
Moises Romero <moises@novacloudware.com>
2017-08-09 05:05:27 -0700

    Increasing code coverage

--- src/argumentTest.js
+++ src/argumentTest.js
@@ -13,4 +13,22 @@ describe('argument', () => {
     done();
   });
 
+  it('should invoke multiple arguments until found', done => {
+    expect(argument(['asdf123123', 'asdf123'], '123')).toBe('123');
+    done();
+  });
+
+  it('should look into env properties', done => {
+    process.env.TESTABC = '123';
+    expect(argument('TESTABC')).toBe('123');
+    done();
+  });
+
+  it('should look into argv as well', done => {
+    process.argv.push('--test=123');
+    expect(argument('test')).toBe('123');
+    expect(argument('test')).toBe('123');
+    done();
+  });
+
 });
